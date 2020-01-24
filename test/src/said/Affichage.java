27ccf7fe16bbcd70413b3e3e34a8e5dd4cb69397
SAID <SAID@DESKTOP-C6A78DB>
2020-01-24 11:08:44 +0100

    hhhhhhh

---
+++ test/src/said/Affichage.java
@@ -0,0 +1,9 @@
+package said;
+
+public class Affichage {
+	public void Suso() {
+		// TODO Auto-generated method stub
+System.out.println("Hello Said");
+	}
+
+}
---
+++ test/src/said/Main.java
@@ -0,0 +1,11 @@
+package said;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Affichage aa = new Affichage();
+aa.Suso();
+	}
+
+}
